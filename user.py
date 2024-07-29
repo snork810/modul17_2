fa7beffa75b5b320a8ab743d84315bb627a739fe
Виктор <110487403+snork810@users.noreply.github.com>
2024-07-29 16:52:36 +0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,14 @@
+from app.backend.database import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from app.models import *
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship('Task', back_populates='users')
